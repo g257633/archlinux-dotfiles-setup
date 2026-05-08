5ca9b3e10fbc1a8806b7fde91819206424ab1ee1
g257633 <g257633@dac.unicamp.br>
2026-05-08 15:17:20 -0300

    Correção do diretório nvim

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "masisz/wisteria.nvim",
+    name = "wisteria",
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "wisteria",
+    },
+  },
+}
